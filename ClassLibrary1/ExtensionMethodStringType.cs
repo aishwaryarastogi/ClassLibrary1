f14668f3c65c65ad023b4fa34695a854d331a036
avi <mitaars@gmail.com>
2018-05-05 22:22:34 +0530

    Add project files.

---
+++ ClassLibrary1/ExtensionMethodStringType.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtClassString
+{
+    public static class ExtensionMethodStringType
+    {
+        public static string Abbreviation(this string input)
+        {
+            string[] word = input.Split(' ');
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i].ToUpper().Equals("OF") || word[i].ToUpper().Equals("AND") || word[i].ToUpper().Equals("THE"))
+                    continue;
+                sb.Append(word[i].Substring(0, 1).ToUpper() + '.');
+            }
+            sb.Remove(sb.Length - 1, 1);
+            return sb.ToString();
+        }
+
+        public static string Palindrome(this string input)
+        {
+            char[] s = input.ToCharArray();
+             s.Reverse();
+            string p = s.ToString();
+            if (input.Equals(p))
+                return "palindrome";
+            else
+                return "not palindrome";
+        }
+
+        public static bool isStringPalindrome(this string input)
+        {
+            var reversed = new string(Enumerable.Range(1, input.Length).Select(i => input[input.Length - i]).ToArray());
+            return String.Compare(input, reversed, true) == 0;
+        }
+
+    }
+}
---
+++ LearningExtensionMethodCall/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtClassString;
+
+
+namespace LearningExtensionMethodCall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            string UserChoice;
+            bool repeatQues;
+            do
+            {
+                Console.WriteLine("Ënter the choice" + '\n' + "1.Abbreviation" + '\n' + "2.Palindrome" +
+                    "\n" + "3.Exit");
+
+                UserChoice = Console.ReadLine();
+                switch (UserChoice)
+                {
+                    case "1":
+                        Console.WriteLine("Enter the Value to extract the abbreviation");
+                        string value = Console.ReadLine();
+                        obj.CallAbbreviation(value);
+                        repeatQues = true;
+
+                        break;
+                    case "2":
+                        Console.WriteLine("Enter the Value to check for palindrome");
+                        string value1 = Console.ReadLine();
+                        obj.isStringPalindrome(value1);
+                        repeatQues = true;
+
+                        break;
+                    case "3":
+
+                        repeatQues = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ënetered Value is out of choice. Please enter a valid choice" +
+                            "from the list of displayed options");
+                        repeatQues = true;
+
+                        break;
+                }
+            } while (repeatQues);
+
+        }
+
+        private void CallAbbreviation(string input)
+        {
+            string abbr = input.Abbreviation();
+            Console.WriteLine(abbr);
+            Console.ReadLine();
+        }
+
+        private void CallPalindrome(string input)
+        {
+            string result = input.Palindrome();
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+
+        private void isStringPalindrome(string input)
+        {
+            bool b = false;
+            if (b == true)
+            {
+                Console.WriteLine("Palindrome");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Not a palindrome");
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
+
